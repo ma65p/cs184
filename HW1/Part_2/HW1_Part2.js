a3456d728aa987d44017e2b86d63250cfaf260f9
Quyen Nguyen <ma65p2004@gmail.com>
2013-06-28 16:37:23 -0500

    Script for Node HW1 Part 2

--- HW1/Part_2/HW1_Part2.js
+++ HW1/Part_2/HW1_Part2.js
@@ -1,38 +1,41 @@
-var limit = 100;
-var count =0;
-var PrimeList = new Array(); 
-var candidate =1;
+function IsPrime(n){
+	if (n==1) {return false;}
+	else if (n<4) {return true;}
+	else if (n%2==0) {return false;}
+	else if (n<9) {return true;}
+	else if (n%3==0) {return false;}
+	else {
+		var r=Math.floor(Math.sqrt(n));
+		var f=5;
+		while (f<=r) {
+			if (n%f==0) {
+				return false;
+				break;
+			}
+			if (n%(f+2)==0) {
+				return false;
+				break;
+			}
+			f=f+6;
+		}
+		return true;
+	}	
+}
+
+var limit = 99;
+var count = 0;
+var PrimeList = new Array();
+PrimeList[0]=2; 
+var candidate = 1;
 while (count < limit) {
-	candidate = candidate + 2;
-	if (prime(candidate)==true) { 
-	PrimeList.push(candidate);
-	count = count+1;
+	candidate = candidate +2;
+	if (IsPrime(candidate)==true) {
+		PrimeList.push(candidate);
+		count = count + 1
 	}
 }
-result = PrimeList
+result = PrimeList;
 
-function prime(n) {
-if (n==1) {return false; }
-else if (n<4) {return true;}
-else if (n%2==0) {return false;}
-else if (n<9) {return true;}
-else if (n%3==0) {return false;}
-else {
-	var r = Math.floor(Math.sqrt(n))
-	var f=5
-	while (f<=r) {
-		if (n%f==0) {
-		return false;
-		break;
-		}
-		if (n%(f+2)==0) {
-		return false;
-		break;
-		}
-	f=f+6;
-	}
-	return true;
-} 
 
 var fs = require('fs');
 var outfile = "Frist100Prime.txt";
