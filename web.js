6b0e17de78ec5ee55d40e54fd59adeff08039033
Ubuntu <ubuntu@ip-172-31-21-165.us-west-2.compute.internal>
2013-07-07 05:33:11 +0000

    re-organize, again

---
+++ web.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express.createServer(express.logger());
+var fs = require('fs');
+app.get('/', function(request, response){
+    fs.readFile('index.html', 'utf8', function(err,data) {
+    response.send(data);})
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
